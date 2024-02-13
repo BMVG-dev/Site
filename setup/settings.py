11f813bbe6292e56722eaf177560f9c2e97fb37f
BMVG-dev <159920423+BMVG-dev@users.noreply.github.com>
2024-02-13 19:52:36 +0000

    Ativando o aplicativo
    setup/settings.py

--- setup/settings.py
+++ setup/settings.py
@@ -36,6 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Ativando o aplicativo
+    'siteBdev.apps.SiteBdevConfig',
 ]
 
 MIDDLEWARE = [
